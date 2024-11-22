6009f6c353b72a28b136d92d34cea5c50fe97aa7
Nikita Vologdins <nvologdins@gmail.com>
2024-11-22 11:50:59 +0200

    pixel perfect

--- src/app/sections/main/Main.tsx
+++ src/app/sections/main/Main.tsx
@@ -2,14 +2,29 @@ import styles from "./main.module.css";
 import Explanation from "../explanation/Explanation";
 import Hints from "../hints/Hints";
 import Limitations from "../limitations/Limitations";
+import Image from "next/image";
+import curveRight from "../../../../public/curve-right.webp";
+import curveLeft from "../../../../public/curve-left.webp";
 
 export default function Main() {
   return (
     <main className={styles.main}>
-      <div className={styles.curved__right}></div>
+      <Image
+        src={curveRight}
+        alt=""
+        width={87}
+        height={202}
+        className={styles.curved__right}
+      />
       <Explanation />
       <Hints />
-      <div className={styles.curved__left}></div>
+      <Image
+        src={curveLeft}
+        alt=""
+        width={94}
+        height={122}
+        className={styles.curved__left}
+      />
       <Limitations />
     </main>
   );
